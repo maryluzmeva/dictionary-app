5c37d73a93952f1e328c8bdf02008d83773b9456
maryluzmeva <maryluzmeva@gmail.com>
2023-11-01 16:08:32 +0100

    Set the Dictionary and Search Component

--- src/App.js
+++ src/App.js
@@ -7,13 +7,11 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
-       </header>
        <main>
         <Dictionary />
        </main>
-       <footer>
+       <footer className="App-footer">
         Coded by 👩🏽‍💻 
         <a href= "https://www.linkedin.com/in/maryluz-mendez-vargas-bbb23824/" target="_blank" rel="noreferrer">Maryluz Mendez Vargas</a> 
         {" "} and is Open-Sourced on {" "}
