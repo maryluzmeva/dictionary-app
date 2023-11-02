f97082fa280e833634b6792411696dda8185cee7
maryluzmeva <maryluzmeva@gmail.com>
2023-11-02 20:14:04 +0100

    Change Header

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
+      <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
        <main>
         <Dictionary />
        </main>
--- src/Results.js
+++ src/Results.js
@@ -1,9 +1,21 @@
 import React from "react";
+import Meaning from "./Meaning";
 
-export default function Result (){
+export default function Results(props) {
+  if (props.results) {
     return (
-        <div className="Results">
-            
-        </div>
-    )
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        {props.results.meanings.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
