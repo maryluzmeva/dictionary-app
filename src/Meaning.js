87a677ce12f3990d86aec810b2b5516e243f4f62
maryluzmeva <maryluzmeva@gmail.com>
2023-11-02 20:20:53 +0100

    Api Calls

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,13 +5,13 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
+      {props.meaning.definitions.map(function (meanings, index) {
         return (
           <div key={index}>
             <p>
-              {definition.definition}
+              {meanings.definition}
               <br />
-              <em>{definition.example}</em>
+              <em>{meanings.example}</em>
             </p>
           </div>
         );
