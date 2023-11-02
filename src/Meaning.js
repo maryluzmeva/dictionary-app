6bb9ecc39db4d7dc1809127a7db6e6127476d820
maryluzmeva <maryluzmeva@gmail.com>
2023-11-02 21:35:46 +0100

    Fixed the API calls

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,21 +1,15 @@
 import React from "react";
 
 export default function Meaning(props) {
-  console.log(props.meanings);
-  return (
-    <div className="Meaning">
-      <h3>{props.meanings.partOfSpeech}</h3>
-      {props.meanings.definitions.map(function (meanings, index) {
-        return (
-          <div key={index}>
-            <p>
-              {meanings.definition}
-              <br />
-              <em>{meanings.example}</em>
-            </p>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
+    console.log(props.meaning);
+    return (
+      <div className="Meaning">
+        <h3>{props.meaning.partOfSpeech}</h3>
+        <strong>Definition: </strong>
+        {props.meaning.definition}
+        <br />
+        <strong>Example: </strong>
+        {props.meaning.example}
+      </div>
+    );
+  }
--- src/Results.js
+++ src/Results.js
@@ -2,20 +2,23 @@ import React from "react";
 import Meaning from "./Meaning";
 
 export default function Results(props) {
-  if (props.results) {
-    return (
-      <div className="Results">
-        <h2>{props.results.word}</h2>
-        {props.results.meanings.map(function (meaning, index) {
-          return (
-            <div key={index}>
-              <Meaning meaning={meaning} />
-            </div>
-          );
-        })}
-      </div>
-    );
-  } else {
-    return null;
+    if (props.results) {
+      return (
+        <div className="Results">
+          <section>
+            <h2>{props.results.word}</h2>
+            <h3>/{props.results.phonetic}/</h3>
+          </section>
+          {props.results.meanings.map(function (meaning, index) {
+            return (
+              <section key={index}>
+                <Meaning meaning={meaning} />
+              </section>
+            );
+          })}
+        </div>
+      );
+    } else {
+      return null;
+    }
   }
-}
