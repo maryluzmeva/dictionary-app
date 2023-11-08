8f77af983aa27041774713815b279631ef54b496
maryluzmeva <maryluzmeva@gmail.com>
2023-11-08 11:20:30 +0100

    Edit Calling Component SearchBar

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import SearchBar from './SearchBar';
+
 
 export default function Header () {
   return (
     <header>
       <h1>Dictionary</h1>
-      <img src="./logo.png" alt="App logo" />
-      <SearchBar />
     </header>
   );
 }
