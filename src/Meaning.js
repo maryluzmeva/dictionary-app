35628febbd299b39476e3db5b70c51a29012ce07
maryluzmeva <maryluzmeva@gmail.com>
2023-11-08 13:25:18 +0100

    CSS

--- src/Meaning.js
+++ src/Meaning.js
@@ -14,10 +14,14 @@ export default function Meaning(props) {
         <strong>Example: </strong>
         {props.meaning.example}
         <br />
+        <section>
         <strong>Synonyms</strong>
         <Synonyms synonyms={props.meaning.synonyms} />
+        </section>
+        <section>
         <strong>Antonyms</strong>
         <Antonyms antonyms={props.meaning.antonyms} />
+        </section>
       </div>
     );
   }
