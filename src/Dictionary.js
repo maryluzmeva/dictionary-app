87cfbd7d807f49174007a11b0b24112c417af140
maryluzmeva <maryluzmeva@gmail.com>
2023-11-09 16:05:22 +0100

    CSS Stzling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,8 +8,12 @@ export default function Dictionary () {
     let [keyword, setKeyword]= useState (" ");
     let [results, setResults]= useState (null);
 
-    function handleResponse (response){
+    function handleResponseDictionary (response){
 			setResults (response.data);
+    }
+
+    function handleResponseImages (response){
+
     }
     function search (event) {
         event.preventDefault ();
@@ -17,7 +21,14 @@ export default function Dictionary () {
         //documentation: https://www.shecodes.io/learn/apis/dictionary
         const apiKey= "c71f439f65td859373faeeba102o0222";
         let apiUrl=`https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-        axios.get (apiUrl).then (handleResponse);
+        axios.get (apiUrl).then (handleResponseDictionary);
+
+        const pexelsApiKey="73blnsRA4d8ecP3RnSW3Ey6nwaZ7IC4UoGjwAawII8WFoao4EcGs6RTe";
+        let pexelsApiUrl= `https://api.pexels.com/v1/search/?page=2&per_page=1&query=${keyword}"`;
+        let headers={"Authorization":`Bearer${pexelsApiKey}`};
+        axios
+          .get (pexelsApiUrl, 
+            {headers:headers}).then (handleResponseImages);
     }
     
     function handleKeywordChange (event){
